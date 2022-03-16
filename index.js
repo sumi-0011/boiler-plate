c42b38d597b7e56f2353d01b7853e04b18fbc813
sumi.byun <selina2000@naver.com>
2022-03-16 23:27:31 +0900

    #12 토큰 생성

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 5000;
 const bodyParser = require("body-parser");
-const { User } = require("./models/User");
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
+const { User } = require("./models/User");
 
-//applications/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extension: true }));
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//applications/json 타입으로 된 것을 분석해서 가져올 수 있게
+// application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -19,22 +20,57 @@ mongoose
   })
   .then(() => console.log("Mongoose Connect...."))
   .catch((err) => console.log(err));
+app.get("/", (req, res) => res.send("Hello World!~~"));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/api/hello", (req, res) => {
+  res.send("안녕하세요~");
 });
 
-app.post("/register", (req, res) => {
-  //회원 가입 할때 필요한 정보들을 client에서 가져오면
-  //그것들을 데이터 베이스에 넣어준다.
-  const user = new User(req.body); //정보들을 user모델에 저장해준다.
-
+app.post("/api/users/register", (req, res) => {
+  // 회원 가입 시 필요한 정보들을 client 에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
-    //status 200 : 성공했다는 표시
-    return res.status(200).json({ success: true });
+    return res.status(200).json({
+      success: true,
+    });
   });
 });
+
+app.post("/api/users/login", (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 았는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+
+    // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+
+      // 비밀번호까지 맞다면 토큰을 생성하기
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지 중 쿠키
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
+const port = 5000;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- models/User.js
+++ models/User.js
@@ -1,20 +1,24 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const myPlaintextPassword = "s0//P4$$w0rD";
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
     type: String,
-    maxlength: 50,
+    maxLength: 50,
   },
   email: {
     type: String,
-    trim: true, //빈칸을 없애줌
-    unique: true, //똑같은 이메일을 쓰지 못하게
+    trim: true,
+    unique: 1,
   },
   password: {
     type: String,
     minLength: 5,
   },
-  lastName: {
+  lastname: {
     type: String,
     maxLength: 50,
   },
@@ -30,6 +34,46 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  if (user.isModified("password")) {
+    // 비밀번호를 암호화 시킨다
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  // plainPassword 123131112, 암호화된 비밀번호 $2b$10$EAYeI2ysE4LV8E8zfRcpiOX/OsOU/1YwtVcMqXobFTu4FX30Akb7u
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+
+  // jsonwebtoken을 이용해서 token을 생성하기
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
 
 const User = mongoose.model("User", userSchema); //모델 이름, 스키마
 
